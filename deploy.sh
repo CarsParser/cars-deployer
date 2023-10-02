1d7a3e86c45ef70a8b5a737d2de28ef5d8b9d23e
Vladimir Gololobov <Vladimir.Gololobov@x5.ru>
2023-10-02 23:07:15 +0300

    fix(): topic count

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ sleep 15;
 wait;
 log "${GREEN} Begin creating topics"
 docker exec $(docker ps -q -f name=kafka | tail -n1) /opt/kafka/bin/kafka-topics.sh --list --zookeeper zookeeper:2181
-docker exec $(docker ps -q -f name=kafka | tail -n1) /opt/kafka/bin/kafka-topics.sh --create --if-not-exists --zookeeper zookeeper:2181 --partitions 10 --replication-factor 1 --topic load_cars
+docker exec $(docker ps -q -f name=kafka | tail -n1) /opt/kafka/bin/kafka-topics.sh --create --if-not-exists --zookeeper zookeeper:2181 --partitions 1 --replication-factor 1 --topic load_cars
 docker exec $(docker ps -q -f name=kafka | tail -n1) /opt/kafka/bin/kafka-topics.sh --create --if-not-exists --zookeeper zookeeper:2181 --partitions 10 --replication-factor 1 --topic cars_notification
 docker exec $(docker ps -q -f name=kafka | tail -n1) /opt/kafka/bin/kafka-topics.sh --list --zookeeper zookeeper:2181
 log "${GREEN} Done creating topics"
